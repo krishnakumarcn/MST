8d69e92ccca6c4ce975d29ddf5a7bbfcbdb380fb
Krishnakumar.C.N <krishnacnskp@gmail.com>
2016-04-12 02:00:55 +0530

    Global variable included for better handling

--- Main.java
+++ Main.java
@@ -4,14 +4,22 @@
  * and open the template in the editor.
  */
 package Kruskals;
+import prim.PrimTest;
 
 /**
  *
  * @author krishnakumar
  */
 public class Main {
-    public static void main(){
-        Graph graph=new Graph();
+    public static String inputfile="input1.txt";
+    public static long timeKruskel,timePrim,numVertex,numEdge;
+    public static void main(String[] arg ){
+       Graph.mainGraph();
+       PrimTest.primStart();
+       System.out.println("-----------------\n\nNo.of Vertices: "+numVertex+"\nNo.of Edges: "+numEdge);
+       System.out.println("\n-----------------\nExecution Time\n----------------\n"
+               + "Kruskel's Algorithm: "+timeKruskel+"ms");
+       System.out.println("Prim's Algorithm: "+timePrim+"ms"+"\n\n");
     }
     
 }
